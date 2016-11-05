3814e52cc3afeb859d00f6af382628b79b9a5d34
Lukasz Taczuk <overfl0@users.noreply.github.com>
2016-11-05 03:38:47 +0100

    Import and call a real function

--- src/python_code/Adapter.py
+++ src/python_code/Adapter.py
@@ -1,4 +1,11 @@
+import importlib
 import traceback
+import sys
+
+# If you want the user modules to be reloaded each time the function is called, set this to True
+PYTHON_MODULE_DEVELOPMENT = False
+
+FUNCTION_CACHE = {}
 
 def format_error_string(stacktrace_str):
     """Return a formatted exception."""
@@ -21,12 +28,50 @@ def parse_input(input_value):
     return eval(input_value)
 # The extension entry point in python
 def python_adapter(input_string):
+    global FUNCTION_CACHE
+
     try:
         if input_string == "":
             return format_error_string("Input string cannot be empty")
 
         real_input = parse_input(input_string)
+        #return format_response_string(input_string)
 
-        return format_response_string(real_input)
+        full_function_name = real_input[0]
+        function_args = real_input[1:]
+
+        try:
+            # Raise dummy exception if needs force-reload
+            if PYTHON_MODULE_DEVELOPMENT:
+                raise KeyError('Dummy KeyError')
+
+            function = FUNCTION_CACHE[full_function_name]
+
+        except KeyError:  # Function not cached, load the module
+            function_path, function_name = full_function_name.rsplit('.', 1)
+
+            try:
+                module = sys.modules[function_path]
+
+            except KeyError:
+                # Module not imported yet, import it
+                #print("Module not imported")
+                module = importlib.import_module(function_path)
+
+            # Force reloading the module if we're developing
+            if PYTHON_MODULE_DEVELOPMENT:
+                importlib.reload(module)
+
+            # Get the requested function
+            function = getattr(module, function_name)
+            FUNCTION_CACHE[full_function_name] = function
+
+        # Call the requested function with the given arguments
+        return_value = function(*function_args)
+        return format_response_string(return_value)
+
     except:
         return format_error_string(traceback.format_exc())
+
+# Somehow Visual Studio cannot load this if there is a newline at The
+# end of the file
