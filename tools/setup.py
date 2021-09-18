c270f5585e410f78711853ff78d97db6585566b6
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-09-18 18:17:47 +0200

    Quick update to the setup.py script

--- tools/setup.py
+++ tools/setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-#######################
-#  Frontline Setup Script  #
-#######################
+######################################################
+#  Pythia Setup Script (the one to be rewritten :P)  #
+######################################################
 
 import os
 import sys
@@ -13,27 +13,27 @@ import winreg
 
 ######## GLOBALS #########
 PROJECTDIR = "@Pythia"
+PROJECTDIR_DEV = PROJECTDIR + "_dev"
 ##########################
 
 def main():
     print("""
-  ###########################################
-  # Frontline Development Environment Setup #
-  ###########################################
+  ########################################
+  # Pythia Development Environment Setup #
+  ########################################
 
-  This script will create your Frontline dev environment for you.
+  This script will create your Pythia dev environment for you.
 
   This script will create two hard links on your system:
-    [Arma 3 Installation folder]   	=> @Pythia
+    [Arma 3 Installation folder]   	=> @Pythia_dev
     [Arma 3 Installation folder]   	=> python
   """)
     print("\n")
 
     try:
         reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-        key = winreg.OpenKey(reg,
-                r"SOFTWARE\Wow6432Node\bohemia interactive\arma 3")
-        armapath = winreg.EnumValue(key,1)[1]
+        key = winreg.OpenKey(reg, r"SOFTWARE\Wow6432Node\bohemia interactive\arma 3")
+        armapath = winreg.EnumValue(key, 1)[1]
     except:
         print("Failed to determine Arma 3 Path.")
         return 1
@@ -41,16 +41,15 @@ def main():
     try:
         # Get the normal user name
         regmission = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        keymission = winreg.OpenKey(regmission,
-                r"SOFTWARE\bohemia interactive\arma 3")
-        armauser = winreg.EnumValue(keymission,0)[1]
+        keymission = winreg.OpenKey(regmission, r"SOFTWARE\bohemia interactive\arma 3")
+        armauser = winreg.EnumValue(keymission, 0)[1]
     except:
         print("Failed to determine Arma 3 User.")
         return 2
 
     scriptpath = os.path.realpath(__file__)
     projectpath = os.path.dirname(os.path.dirname(scriptpath))
-    pythonpath = os.path.join(armapath,"python")
+    pythonpath = os.path.join(armapath, "python")
 
     print("# Detected Paths:")
     print("  Arma Path:     {}".format(armapath))
@@ -66,7 +65,8 @@ def main():
         if not os.path.exists(pythonpath):
             os.mkdir(pythonpath)
 
-        subprocess.check_call(["cmd", "/c", "mklink", "/J", os.path.join(armapath, PROJECTDIR), os.path.join(projectpath, PROJECTDIR)])
+        subprocess.check_call(["cmd", "/c", "mklink", "/J", os.path.join(armapath, PROJECTDIR_DEV),
+                               os.path.join(projectpath, PROJECTDIR)])
 
     except:
         print("Something went wrong during the link creation. Please finish the setup manually.")
