05af9e126a4f2ea5f3ab388e8d7a65c4ec3d5da0
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-09-18 14:49:12 +0200

    Remove stdafx

--- src/Pythia/stdafx.h
+++ src/Pythia/stdafx.h
@@ -1,12 +1,7 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
 #ifdef _WIN32
-#include "targetver.h"
+#include <SDKDDKVer.h>
 
 //#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
--- src/PythiaSetPythonPath/stdafx.h
+++ src/PythiaSetPythonPath/stdafx.h
@@ -1,17 +1,9 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
 #ifdef _WIN32
-#include "targetver.h"
+#include <SDKDDKVer.h>
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
 #endif
-
-
-// TODO: reference additional headers your program requires here
