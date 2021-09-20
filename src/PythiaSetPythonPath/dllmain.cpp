975db2357dc9de4eaf1410df0aa77ce0bcc782c7
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-09-21 01:30:59 +0200

    Adding dummy PythiaSetPythonPath that actually compiles and runs

--- src/PythiaSetPythonPath/dllmain.cpp
+++ src/PythiaSetPythonPath/dllmain.cpp
@@ -2,49 +2,78 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <tchar.h>
 #include <string>
 #include "../Pythia/Logger.h"
 #include "../Pythia/Paths.h"
 #include "../Pythia/common.h"
 
-#define LOGGER_FILENAME L"PythiaSetPythonPath.log"
+#define LOGGER_FILENAME LITERAL("PythiaSetPythonPath.log")
 
-std::shared_ptr<spdlog::logger> Logger::logfile = getFallbackLogger();
+std::shared_ptr<spdlog::logger> Logger::logfile = getFallbackLogger();  // TODO: Changeme
 std::wstring pythonPath = L"<Not set>";
 
 void setDLLPath()
 {
     LOG_INFO("Setting DLL path");
-    std::wstring pythonPath = getPythonPath();
+    tstring pythonPath = getPythonPath();
 
+    #ifdef _WIN32
     LOG_INFO(std::string("Setting DLL path to: ") + Logger::w2s(pythonPath));
     if (SetDllDirectory(pythonPath.c_str()) == 0)
     {
         LOG_ERROR("Failed to call SetDllDirectory");
     }
+    #else
+    LOG_INFO("Not changing any paths, for now, on linux. #TODO");
+    #endif
 }
 
 extern "C"
 {
-    __declspec (dllexport) void __stdcall RVExtension(char *output, int outputSize, const char *input);
-    __declspec (dllexport) void __stdcall RVExtensionVersion(char *output, int outputSize);
+#ifdef _WIN32
+    __declspec(dllexport) void __stdcall RVExtension(char* output, int outputSize, const char* input);
+    __declspec(dllexport) void __stdcall RVExtensionVersion(char* output, int outputSize);
+#else
+#define __stdcall
+    __attribute__((visibility("default"))) void RVExtension(char* output, int outputSize, const char* input);
+    __attribute__((visibility("default"))) void RVExtensionVersion(char* output, int outputSize);
+#endif
 }
 
 void __stdcall RVExtension(char *output, int outputSize, const char *input)
 {
     std::string str = Logger::w2s(pythonPath);
-    size_t minSize = min((size_t)outputSize, str.size() + 1);
+    size_t minSize = std::min<size_t>((size_t)outputSize, str.size() + 1);
     snprintf(output, minSize, "%s", str.c_str());
 }
 
 void __stdcall RVExtensionVersion(char *output, int outputSize)
 {
     std::string versionInfo(PYTHIA_VERSION);
-    size_t minSize = min((size_t)outputSize, versionInfo.size() + 1);
+    size_t minSize = std::min<size_t>((size_t)outputSize, versionInfo.size() + 1);
     snprintf(output, minSize, "%s", versionInfo.c_str());
 }
 
+#ifndef _WIN32
+__attribute__((constructor))
+#endif
+void libraryLoad()
+{
+    createLogger("PythiaSetPythonPathLogger", LOGGER_FILENAME);
+    setDLLPath();
+    LOG_FLUSH();
+}
+
+#ifndef _WIN32
+__attribute__((destructor))
+#endif
+void libraryUnload()
+{
+    LOG_FLUSH();
+    spdlog::drop_all();
+}
+
+#ifdef _WIN32
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -53,16 +82,15 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        createLogger("PythiaSetPythonPathLogger", LOGGER_FILENAME);
-        setDLLPath();
-        LOG_FLUSH();
+        libraryLoad();
         break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
+        break;
     case DLL_PROCESS_DETACH:
-        LOG_FLUSH();
-        spdlog::drop_all();
+        libraryUnload();
         break;
     }
     return TRUE;
 }
+#endif
