0f300ce077c98f44ca0d7b719de502f0101d03d3
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-10-06 02:37:29 +0200

    Implement install_requirements.sh

---
+++ @Pythia/install_requirements.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#############################################################################
+# This script will install any python dependencies that will be needed
+# by any Pythia code.
+#
+# To install the dependencies for a plugin, simply drag a requirements.txt
+# file onto install_requirements.sh
+#############################################################################
+
+for interpreter in `dirname "$0"`/python-*-embed-linux*
+do
+    echo ===============================================================================
+    echo Installing requirements for $interpreter from "$1"...
+    echo ===============================================================================
+
+    "${interpreter}"/bin/python3 -m pip install --upgrade -r "$1"
+
+    if [ $? -ne 0 ]; then
+        echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        echo An error happened during requirements installation. Your python environment is
+        echo now in an undefined state!
+        echo Fix the issues and reinstall the requirements!
+        echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        exit 1
+    fi
+done
