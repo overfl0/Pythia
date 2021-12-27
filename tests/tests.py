16155fc7736af49d7273ed64bb9bf39fc0967d3d
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-12-27 02:09:19 +0100

    Pythia works when run from a directory with non-ascii names

--- tests/tests.py
+++ tests/tests.py
@@ -39,21 +39,21 @@ class Base(unittest.TestCase):
     def create_request(function, args):
         return f'["{function}", {args}]'
 
-    def setUp(self):
-        self.maxDiff = 3000
+    def ensure_no_tester(self):
         try:
             os.remove(os.path.join(self.this_dir, self.pythia_tester))
         except FileNotFoundError:
             pass
+
+    def setUp(self):
+        self.maxDiff = 3000
+        self.ensure_no_tester()
 
         tester_path = os.path.join(self.this_dir, '..', '@Pythia', self.pythia_tester)
         shutil.copy2(tester_path, self.this_dir)
 
     def tearDown(self):
-        try:
-            os.remove(os.path.join(self.this_dir, self.pythia_tester))
-        except FileNotFoundError:
-            pass
+        self.ensure_no_tester()
 
 
 class TestBasicPing(Base):
@@ -66,7 +66,7 @@ class TestBasicPing(Base):
     def test_sanity_can_open_with_pythia_dir(self):
         request = self.create_request('pythia.ping', [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
         output, err, code = self._call_tester(self.pythia_path, request)
-        self.assertEqual(code, 0, 'Calling the tester with the right path succeed')
+        self.assertEqual(code, 0, 'Calling the tester with the right path should succeed')
         self.assertEqual(output, '["r",[1,2,3,4,5,6,7,8,9,0]]')
 
 
@@ -75,15 +75,61 @@ class TestMods(Base):
         request = self.create_request('basic.function', [1, 2, 3])
         output, err, code = self._call_tester(self.pythia_path, request,
                                               loaded_pbos=[os.path.join('@BasicMod', 'addons', 'basic_mod.pbo')])
-        self.assertEqual(code, 0, 'Calling the tester with the right path succeed')
+        self.assertEqual(code, 0, 'Calling the tester with the right path should succeed')
         self.assertEqual(output, '["r",[1,2,3]]')
 
     def test_renamed_loaded_mod(self):
         request = self.create_request('renamed.function', [1, 2, 3, 4])
         output, err, code = self._call_tester(self.pythia_path, request,
                                               loaded_pbos=[os.path.join('@RenamedMod', 'addons', 'renamed_mod.pbo')])
-        self.assertEqual(code, 0, 'Calling the tester with the right path succeed')
+        self.assertEqual(code, 0, 'Calling the tester with the right path should succeed')
         self.assertEqual(output, '["r",[1,2,3,4]]')
+
+    # def test_special_chars_loaded_mod(self):
+    #     request = self.create_request('żółw.function', [1, 2, 3, 4, 5])
+    #     output, err, code = self._call_tester(self.pythia_path, request,
+    #                                           loaded_pbos=[os.path.join('@ŻółwMod', 'addons', 'żółw_mod.pbo')])
+    #     self.assertEqual(code, 0, 'Calling the tester with the right path should succeed')
+    #     self.assertEqual(output, '["r",[1,2,3,4,5]]')
+
+
+class TestSpecialCharsPythia(Base):
+    special_chars_pythia_path = '@ŻółwPythia'
+
+    def delete_link(self):
+        try:
+            os.remove(os.path.abspath(self.special_chars_pythia_path))
+        except (FileNotFoundError, PermissionError, IsADirectoryError):
+            pass
+
+        # Linux symlink + Windows junction
+        try:
+            os.rmdir(os.path.abspath(self.special_chars_pythia_path))
+        except FileNotFoundError:
+            pass
+
+    def make_link(self, existing_directory, new_name):
+        if platform.system() == 'Windows':
+            cmd = ['cmd', '/c', 'mklink', '/J', new_name, existing_directory]
+            subprocess.run(cmd, check=True, cwd=self.this_dir)
+        else:  # Linux
+            os.symlink(existing_directory, new_name)
+
+    def setUp(self):
+        super().setUp()
+        self.delete_link()
+        self.make_link(os.path.join(self.this_dir, self.pythia_path), os.path.abspath(self.special_chars_pythia_path))
+
+    def tearDown(self):
+        super().tearDown()
+        self.delete_link()
+
+    def test_pythia_in_directory_with_special_chars(self):
+        request = self.create_request('basic.function', [1, 2])
+        output, err, code = self._call_tester(self.special_chars_pythia_path, request,
+                                              loaded_pbos=[os.path.join('@BasicMod', 'addons', 'basic_mod.pbo')])
+        self.assertEqual(code, 0, 'Calling the tester with the right path should succeed')
+        self.assertEqual(output, '["r",[1,2]]')
 
 
 if __name__ == '__main__':
