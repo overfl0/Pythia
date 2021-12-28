fc79e538bd8643ccb6878072912d53bcccdfe9a0
Lukasz Taczuk <overfl0@users.noreply.github.com>
2021-12-28 19:10:49 +0100

    Use a vendored libcrypt

--- tools/convert_python_build.sh
+++ tools/convert_python_build.sh
@@ -16,6 +16,10 @@ pushd python/install/
 rm -rf lib/python*/test
 find -name '*.a' -delete
 #zip -r "${1%-*}.zip" bin include lib share
+# Get the original libcrypt.so from a manylinux docker
+pushd lib
+docker run --platform linux/386 -v "$(pwd)"/:/data quay.io/pypa/manylinux2014_i686:latest /bin/bash -c "cp /usr/local/lib/libcrypt.so.1 /data/ && chown 1000:1000 /data/libcrypt.so.1 && chmod 555 /data/libcrypt.so.1"
+popd
 tar -jcvf "${1%-*}.tbz" bin include lib share
 mv "${1%-*}.tbz" ../..
 popd
