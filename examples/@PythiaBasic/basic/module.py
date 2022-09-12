17b08469e28fe485531adea80e49dea9968dc2e9
Lukasz Taczuk <overfl0@users.noreply.github.com>
2022-09-12 22:06:05 +0200

    Add example to call modules directly

---
+++ examples/@PythiaBasic/basic/module.py
@@ -0,0 +1,8 @@
+def ping_from_module(*args):
+    """
+    You can also call functions directly from modules, not just packages!
+    Returns all the arguments passed to the function
+    To execute this function, call:
+    ["basic.module.ping_from_module", ["string", 1, 2.3, true]] call py3_fnc_callExtension
+    """
+    return args
