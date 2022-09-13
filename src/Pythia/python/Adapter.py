d33554530a63774837fbb3537aff64417333565a
Lukasz Taczuk <overfl0@users.noreply.github.com>
2022-09-14 00:32:05 +0200

    Use recursive getattr to access python objects

--- src/Pythia/python/Adapter.py
+++ src/Pythia/python/Adapter.py
@@ -127,7 +127,7 @@ def import_and_strip_traceback(full_module_name):
         return importlib.import_module(module_name)
 
     except Exception as ex:
-        spec = importlib.util.find_spec(module_name)
+        spec = importlib.util.find_spec(module_name)  # FIXME: can raise ModuleNotFoundError here!
         if spec is None:
             # if the module is not found, then do not print traceback at all
             count = 0
@@ -169,42 +169,73 @@ def python_adapter(sqf_args):
         if not isinstance(function_args, list):
             raise PythiaExecuteException('The arguments of a function need to be passed in an array')
 
-        try:
-            # Raise dummy exception if needs force-reload
-            if PYTHON_MODULE_DEVELOPMENT:
-                if full_function_name not in PYTHIA_INTERNAL_FUNCTIONS:
-                    raise KeyError('Dummy KeyError')
+        #######################################################################
+        # Access the function/method ##########################################
+        #######################################################################
 
-            function = FUNCTION_CACHE[full_function_name]
-
-        except KeyError:  # Function not cached, load the module
-            function_path, function_name = full_function_name.rsplit('.', 1)
-
-            # Prevent calling arbitrary python functions
-            base_module = function_path.split('.')[0]
-            if(base_module != 'python' and
-               base_module != 'pythia' and
-               base_module not in PythiaModuleWrapper.modules):
-                message = 'Pythia module \'{}\' not recognized! Have you loaded the right Arma mods?'
-                raise PythiaImportException(message.format(base_module))
-
+        if full_function_name in PYTHIA_INTERNAL_FUNCTIONS:
+            function = PYTHIA_INTERNAL_FUNCTIONS[full_function_name]
+        else:
+            # Recursively call getattr on elements. If not present, try loading it as a module
+            attributes = full_function_name.split('.')
+            base_module_name = attributes.pop(0)
             try:
-                module = sys.modules[function_path]
-
+                element = sys.modules[base_module_name]
             except KeyError:
                 # Module not imported yet, import it
-                module = import_and_strip_traceback(function_path)
+                element = import_and_strip_traceback(base_module_name)
 
-            # Force reloading the module if we're developing
-            if PYTHON_MODULE_DEVELOPMENT:
-                importlib.reload(module)
+            for depth, attribute in enumerate(attributes):
+                try:
+                    element = getattr(element, attribute)
+                except AttributeError as ex:
+                    # Sanity check: if element is not a module attribute is just missing and not a submodule
+                    if type(element).__name__ != 'module':
+                        raise
 
-            try:
-                # Get the requested function
-                function = getattr(module, function_name)
-            except AttributeError as ex:
-                raise PythiaImportException(str(ex))
-            FUNCTION_CACHE[full_function_name] = function
+                    # No such element. It may be a module, it may be a regular item
+                    full_path = '.'.join([base_module_name] + attributes[:depth + 1])
+                    element = import_and_strip_traceback(full_path)
+
+            function = element
+
+        # try:
+        #     # Raise dummy exception if needs force-reload
+        #     if PYTHON_MODULE_DEVELOPMENT:
+        #         if full_function_name not in PYTHIA_INTERNAL_FUNCTIONS:
+        #             raise KeyError('Dummy KeyError')
+        #
+        #     # TODO: Change me here
+        #     function = FUNCTION_CACHE[full_function_name]
+        #
+        # except KeyError:  # Function not cached, load the module
+        #     function_path, function_name = full_function_name.rsplit('.', 1)
+        #
+        #     # Prevent calling arbitrary python functions
+        #     base_module = function_path.split('.')[0]
+        #     if(base_module != 'python' and
+        #        base_module != 'pythia' and
+        #        base_module not in PythiaModuleWrapper.modules):
+        #         message = 'Pythia module \'{}\' not recognized! Have you loaded the right Arma mods?'
+        #         raise PythiaImportException(message.format(base_module))
+        #
+        #     try:
+        #         module = sys.modules[function_path]
+        #
+        #     except KeyError:
+        #         # Module not imported yet, import it
+        #         module = import_and_strip_traceback(function_path)
+        #
+        #     # Force reloading the module if we're developing
+        #     if PYTHON_MODULE_DEVELOPMENT:
+        #         importlib.reload(module)
+        #
+        #     try:
+        #         # Get the requested function
+        #         function = getattr(module, function_name)
+        #     except AttributeError as ex:
+        #         raise PythiaImportException(str(ex))
+        #     FUNCTION_CACHE[full_function_name] = function
 
         retval = handle_function_calling(function, function_args)
 
